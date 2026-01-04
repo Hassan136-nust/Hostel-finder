e9bc4d0dfe9ebbb9210ffa8569b8f96de5cb166d
Hussain Jamal <Hjamal9865@gmail.com>
2026-01-05 04:15:11 +0500

    HOSTEL model added

---
+++ server/models/hostel.model.ts
@@ -0,0 +1,50 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+export interface IHostel extends Document {
+  name: string;
+  address: string;
+  city: string;
+  description: string;
+  owner: mongoose.Types.ObjectId;
+  type: "Boys" | "Girls" | "Family";
+  images: {
+    public_id: string;
+    url: string;
+  }[];
+  facilities: string[];
+  contactPhone: string;
+
+  reviews: mongoose.Types.ObjectId[]; 
+  questions: mongoose.Types.ObjectId[]; 
+  rating: number; 
+}
+
+const hostelSchema: Schema<IHostel> = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    description: { type: String, required: true },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: { type: String, enum: ["Boys", "Girls", "Family"], required: true },
+    images: [{ public_id: String, url: String }],
+    facilities: [{ type: String }],
+    contactPhone: { type: String, required: true },
+
+    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
+    questions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Question" }],
+
+    rating: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const hostelModel: Model<IHostel> = mongoose.model<IHostel>(
+  "Hostel",
+  hostelSchema
+);
+export default hostelModel;
